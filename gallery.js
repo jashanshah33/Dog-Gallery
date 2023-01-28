c92414ce6952050c2517f585063b82d30469afac
jashanshah33 <jashanshah33@gmail.com>
2023-01-28 05:00:22 -0500

    Error fixed
    Signed-off-by: jashanshah33 <jashanshah33@gmail.com>

--- gallery.js
+++ gallery.js
@@ -1,63 +1,64 @@
-var dropdown = $('#breed');
+var dropdown = $("#breed");
 var breed;
 
-$.get("https://dog.ceo/api/breeds/list/all", 
-function (data) {
-    let breeds = data.message
-    for (const breed in breeds) {
-        dropdown.append(`
+$.get("https://dog.ceo/api/breeds/list/all", function (data) {
+  let breeds = data.message;
+  for (const breed in breeds) {
+    dropdown.append(`
        <option id="breedValue" value="${breed}">${breed}</option>
-       `)
-    }
-},
+       `);
+  }
+});
 
-);
+dropdown.change(function () {
+  let breed = dropdown.val();
 
-dropdown.change(function () { 
-    let breed = dropdown.val();
+  if (breed) {
     let url = "https://dog.ceo/api/breed/" + breed + "/list";
 
     $("#dog-sub-breeds").remove();
 
-        $.get(url, function (data) {
-            if (data.message.length !== 0 ) {
-                let subBreeds = data.message;
+    $.get(url, function (data) {
+      let subBreedArray;
+      let subBreeds = data.message;
+      for (let subBreed in subBreeds) {
+        subBreedArray = subBreeds[subBreed];
+      }
 
-                dropdown.after(`<select id="dog-sub-breeds"> </select>`);
-                var sunDropdown = $('#dog-sub-breeds') 
+      if (subBreedArray.length !== 0) {
+        dropdown.after(`<select id="dog-sub-breeds"> </select>`);
+        var sunDropdown = $("#dog-sub-breeds");
 
-                for (const sunBreed of subBreeds) {
-                    sunDropdown.append(`<option id="breedValue" value="${sunBreed}">${sunBreed}</option> `)
-
-                }
-            } 
-        })
-
-    
+        for (const subBreed of subBreedArray) {
+          sunDropdown.append(
+            `<option id="breedValue" value="${subBreed}">${subBreed}</option> `
+          );
+        }
+      }
+    });
+  }
 });
 
+$("#img-btn").click(function (e) {
+  e.preventDefault();
 
-$('#img-btn').click(function (e) {
-    e.preventDefault();
+  $("#bottom img").remove();
 
-    $('#bottom img').remove();
+  let breed = dropdown.val();
+  let subBreed = $("#dog-sub-breeds").val();
 
-    let breed = dropdown.val();
-    let subBreed = $("#dog-sub-breeds").val();
+  let url = "https://dog.ceo/api/breed/" + breed;
+  if (subBreed !== undefined) {
+    url += "/" + subBreed;
+  }
 
-    let url = "https://dog.ceo/api/breed/" + breed;
-    if(subBreed !== undefined) {
-        url += "/" + subBreed
-    }   
-    
-        url += "/images";
-        
-    
-    $.get(url, function (data) {
-        let imagesUrl = data.message;
+  url += "/images";
 
-        for (let imageUrl of imagesUrl) {
-            $('#bottom').append('<img src="' + imageUrl + '" alt="' + breed + '">');
-        }
-    });
-})
+  $.get(url, function (data) {
+    let imagesUrl = data.message;
+
+    for (let imageUrl of imagesUrl) {
+      $("#bottom").append('<img src="' + imageUrl + '" alt="' + breed + '">');
+    }
+  });
+});
